76072fd5238c2b0c1721aa7bd83a0e264bbc6159
K2D2021 <zxtreme@yandex.ru>
2022-10-31 22:43:30 +0300

    Add binding in MainActivity.kt

--- app/src/main/java/com/example/k2dbinarywatch/presentation/MainActivity.kt
+++ app/src/main/java/com/example/k2dbinarywatch/presentation/MainActivity.kt
@@ -2,11 +2,20 @@ package com.example.k2dbinarywatch.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.k2dbinarywatch.R
+import com.example.k2dbinarywatch.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.hoursNumbers.text = ""
+        binding.minutesNumbers.text = ""
+        binding.secondsNumbers.text = ""
     }
+
+
 }
