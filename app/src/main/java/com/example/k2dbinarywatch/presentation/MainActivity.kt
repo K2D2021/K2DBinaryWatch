c320e02e1c10bfcde312471745d94f78c6e3a880
K2D2021 <zxtreme@yandex.ru>
2022-11-02 22:47:58 +0300

    Add coroutines dependencies.

--- app/src/main/java/com/example/k2dbinarywatch/presentation/MainActivity.kt
+++ app/src/main/java/com/example/k2dbinarywatch/presentation/MainActivity.kt
@@ -2,10 +2,11 @@ package com.example.k2dbinarywatch.presentation
 
 import android.annotation.SuppressLint
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.k2dbinarywatch.databinding.ActivityMainBinding
+import kotlinx.coroutines.delay
 import java.text.SimpleDateFormat
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
@@ -20,8 +21,37 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val myDate = updateTime()
 
-        val myDate = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        var binaryHours = myDate[0].toString()
+        binaryHours += myDate[1]
+        var binaryMinutes = myDate[3].toString()
+        binaryMinutes += myDate[4]
+        var binarySeconds = myDate[6].toString()
+        binarySeconds += myDate[7]
+//        Toast.makeText(this, myDate, Toast.LENGTH_SHORT).show()
+        binding.hoursNumbers.text = binaryHours.toInt().toString(2)
+        binding.minutesNumbers.text = binaryMinutes.toInt().toString(2)
+        binding.secondsNumbers.text = binarySeconds.toInt().toString(2)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+
+        suspend {
+            delay(1000)
+            binding.secondsNumbers.text = "HI"
+            Toast.makeText(this, "1", Toast.LENGTH_SHORT).show()
+
+
+        }
+
+
+    }
+
+    fun updateTime(): String {
+        val updatedTime = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val current = LocalDateTime.now()
             val formatter = DateTimeFormatter.ofPattern("HH:mm:ss")
             current.format(formatter)
@@ -31,16 +61,7 @@ class MainActivity : AppCompatActivity() {
             formatter.format(date)
 
         }
-        var binaryHours = myDate[0].toString()
-        binaryHours += myDate[1]
-        var binaryMinutes = myDate[3].toString()
-        binaryMinutes += myDate[4]
-        var binarySeconds = myDate[6].toString()
-        binarySeconds += myDate[7]
-        Toast.makeText(this, myDate, Toast.LENGTH_SHORT).show()
-        binding.hoursNumbers.text = binaryHours.toInt().toString(2)
-        binding.minutesNumbers.text = binaryMinutes.toInt().toString(2)
-        binding.secondsNumbers.text = binarySeconds.toInt().toString(2)
+        return updatedTime
     }
 
 }
