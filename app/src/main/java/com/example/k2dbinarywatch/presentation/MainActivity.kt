9d5157f3dd733d66b1a354b55bae75a2a5ee7764
K2D2021 <zxtreme@yandex.ru>
2022-11-02 23:23:07 +0300

    Make it works. Show time in live format.

--- app/src/main/java/com/example/k2dbinarywatch/presentation/MainActivity.kt
+++ app/src/main/java/com/example/k2dbinarywatch/presentation/MainActivity.kt
@@ -3,10 +3,11 @@ package com.example.k2dbinarywatch.presentation
 import android.annotation.SuppressLint
 import android.os.Build
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.k2dbinarywatch.databinding.ActivityMainBinding
+import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import java.text.SimpleDateFormat
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
@@ -15,53 +16,49 @@ import java.util.*
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
-    @SuppressLint("SimpleDateFormat")
+    @SuppressLint("SimpleDateFormat", "SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val myDate = updateTime()
-
-        var binaryHours = myDate[0].toString()
-        binaryHours += myDate[1]
-        var binaryMinutes = myDate[3].toString()
-        binaryMinutes += myDate[4]
-        var binarySeconds = myDate[6].toString()
-        binarySeconds += myDate[7]
-//        Toast.makeText(this, myDate, Toast.LENGTH_SHORT).show()
-        binding.hoursNumbers.text = binaryHours.toInt().toString(2)
-        binding.minutesNumbers.text = binaryMinutes.toInt().toString(2)
-        binding.secondsNumbers.text = binarySeconds.toInt().toString(2)
-    }
-
-    override fun onResume() {
-        super.onResume()
 
 
-        suspend {
-            delay(1000)
-            binding.secondsNumbers.text = "HI"
-            Toast.makeText(this, "1", Toast.LENGTH_SHORT).show()
+        val scope = MainScope()
+        scope.launch {
 
+            while (true) {
+                delay(1000)
+                val myDate = updateTime()
+
+                var binaryHours = myDate[0].toString()
+                binaryHours += myDate[1]
+                var binaryMinutes = myDate[3].toString()
+                binaryMinutes += myDate[4]
+                var binarySeconds = myDate[6].toString()
+                binarySeconds += myDate[7]
+                binding.hoursNumbers.text = binaryHours.toInt().toString(2)
+                binding.minutesNumbers.text = binaryMinutes.toInt().toString(2)
+                binding.secondsNumbers.text = binarySeconds.toInt().toString(2)
+            }
 
         }
-
-
     }
 
-    fun updateTime(): String {
-        val updatedTime = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val current = LocalDateTime.now()
-            val formatter = DateTimeFormatter.ofPattern("HH:mm:ss")
-            current.format(formatter)
-        } else {
-            val date = Date()
-            val formatter = SimpleDateFormat("HH:mma")
-            formatter.format(date)
 
-        }
-        return updatedTime
+}
+
+@SuppressLint("SimpleDateFormat")
+fun updateTime(): String {
+    val updatedTime = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        val current = LocalDateTime.now()
+        val formatter = DateTimeFormatter.ofPattern("HH:mm:ss")
+        current.format(formatter)
+    } else {
+        val date = Date()
+        val formatter = SimpleDateFormat("HH:mma")
+        formatter.format(date)
+
     }
-
+    return updatedTime
 }
