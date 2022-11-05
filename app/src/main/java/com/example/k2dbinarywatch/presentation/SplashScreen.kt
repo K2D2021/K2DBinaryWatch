beff48a2552dee89b1c5e32d3a5acebc3ae0e4a3
K2D2021 <zxtreme@yandex.ru>
2022-11-05 12:25:11 +0300

    Start adding splashScreen

---
+++ app/src/main/java/com/example/k2dbinarywatch/presentation/SplashScreen.kt
@@ -0,0 +1,18 @@
+package com.example.k2dbinarywatch.presentation
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val intent = Intent(this, MainActivity::class.java).apply {
+        }
+        startActivity(intent)
+        finish()
+    }
+}
