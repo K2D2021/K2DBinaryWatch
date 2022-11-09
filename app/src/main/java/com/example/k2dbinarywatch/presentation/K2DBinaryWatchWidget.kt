c56910b15ca4515e3d77e7bec105a273bb0f0e7c
K2D2021 <zxtreme@yandex.ru>
2022-11-10 00:01:47 +0300

    Fix white screen after click on widget.

--- app/src/main/java/com/example/k2dbinarywatch/presentation/K2DBinaryWatchWidget.kt
+++ app/src/main/java/com/example/k2dbinarywatch/presentation/K2DBinaryWatchWidget.kt
@@ -25,7 +25,7 @@ class K2DBinaryWatchWidget : AppWidgetProvider() {
         // There may be multiple widgets active, so update all of them
         for (appWidgetId in appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId)
-            val intent = Intent(context, MainActivity::class.java)
+            val intent = Intent(context, SplashScreen::class.java)
             val pendingIntent = PendingIntent.getActivity(context, 0, intent, 0)
 
             val views = RemoteViews(context.packageName, R.layout.k2_d_binary_watch_widget)
