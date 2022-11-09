f7d93b079586d0356071dd93c088a6bbafb87396
K2D2021 <zxtreme@yandex.ru>
2022-11-09 23:17:37 +0300

    Add click on widget to open the app.

--- app/src/main/java/com/example/k2dbinarywatch/presentation/K2DBinaryWatchWidget.kt
+++ app/src/main/java/com/example/k2dbinarywatch/presentation/K2DBinaryWatchWidget.kt
@@ -1,14 +1,17 @@
 package com.example.k2dbinarywatch.presentation
 
+import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
+import android.content.Intent
 import android.widget.RemoteViews
 import com.example.k2dbinarywatch.R
 import com.example.k2dbinarywatch.domain.updateTime
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+
 
 /**
  * Implementation of App Widget functionality.
@@ -22,6 +25,12 @@ class K2DBinaryWatchWidget : AppWidgetProvider() {
         // There may be multiple widgets active, so update all of them
         for (appWidgetId in appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId)
+            val intent = Intent(context, MainActivity::class.java)
+            val pendingIntent = PendingIntent.getActivity(context, 0, intent, 0)
+
+            val views = RemoteViews(context.packageName, R.layout.k2_d_binary_watch_widget)
+            views.setOnClickPendingIntent(R.id.appwidget_top, pendingIntent)
+            appWidgetManager.updateAppWidget(appWidgetId, views);
         }
     }
 
